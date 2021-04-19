2787a63d60307b025068c4b03a142cb55762ee5e
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-04-19 20:36:26 +0800

    projectile now spawns in intervals

--- Assets/Scripts/Boss/ProjectileSpawner.cs
+++ Assets/Scripts/Boss/ProjectileSpawner.cs
@@ -1,23 +1,32 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 namespace Boss
 {
     public class ProjectileSpawner : MonoBehaviour
     {
-        [SerializeField] private GameObject projectile;
+        [SerializeField] private GameObject prefab;
+        [SerializeField] private float delayTime;
         [SerializeField] private Transform[] spawnPositions;
-
 
         private void Start()
         {
-            Spawn(spawnPositions[0]);
+            StartCoroutine(SpawnCoroutine(prefab, spawnPositions, delayTime));
         }
 
-        private void Spawn(Transform spawnPosition)
+        public void Spawn(GameObject projectile,Transform spawnPosition)
         {
             Instantiate(projectile, spawnPosition.position, Quaternion.identity);
         }
-        
+
+        private IEnumerator SpawnCoroutine(GameObject projectile, Transform[] positions, float interval)
+        {
+            foreach (var position in positions)
+            {
+                yield return new WaitForSeconds(interval);
+                Instantiate(projectile, position.position, Quaternion.identity);
+            }
+        }
     }
 }
