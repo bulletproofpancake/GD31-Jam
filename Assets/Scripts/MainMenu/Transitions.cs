999b28966ac9cb0118700c09e4c4500e1d0e310c
Georson Chua <71176893+zerogeorson@users.noreply.github.com>
2021-04-22 07:27:28 +0800

    Transition Effect
    Not loading screen, but literally just a transition effect

---
+++ Assets/Scripts/MainMenu/Transitions.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Transitions : MonoBehaviour
+{
+    public Animator animator;
+    public float time;
+
+ 
+    public void LoadScene(string sceneName)
+    {
+        //FindObjectOfType<AudioManager>().Play("button");
+        StartCoroutine(LoadLevel(sceneName));
+       
+
+    }
+
+    IEnumerator LoadLevel(string sceneName)
+    {
+        animator.SetTrigger("End");
+        yield return new WaitForSeconds(time);
+        SceneManager.LoadScene(sceneName);
+    }
+
+}
