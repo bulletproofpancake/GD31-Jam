adb7f80a2c38869dcd182d6a86e623d39494eb93
bulletproofpancake <57520402+bulletproofpancake@users.noreply.github.com>
2021-04-22 22:36:36 +0800

    harpoons now shoot on raycast

---
+++ Assets/Scripts/Boss/Projectiles/BossAttack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using UnityEngine;
+
+namespace Boss.Projectiles
+{
+    public class BossAttack : MonoBehaviour
+    {
+        [SerializeField] private Transform eyes;
+        [SerializeField] private ProjectileSpawner harpoonSpawner;
+        [SerializeField] private float harpoonRange;
+        [SerializeField] private LayerMask playerLayer;
+        
+        private void Update()
+        {
+            ShootHarpoon();
+        }
+
+        void ShootHarpoon()
+        {
+            var ray = new Ray(eyes.position, Vector3.left);
+            Debug.DrawRay(ray.origin, ray.direction*harpoonRange, Color.green);
+            if (Physics.Raycast(ray,harpoonRange,playerLayer))
+            {
+                Debug.DrawRay(ray.origin, ray.direction*harpoonRange, Color.red);
+                harpoonSpawner.isActive = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/Boss/Projectiles/ProjectileSpawner.cs
+++ Assets/Scripts/Boss/Projectiles/ProjectileSpawner.cs
@@ -5,16 +5,19 @@ public class ProjectileSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
     [SerializeField] private Transform[] spawnPoints;
-    [SerializeField] private float cooldown, shotInterval;
+    [SerializeField] private float shotInterval;
     [SerializeField] private bool tracksPlayer;
     
     public bool isActive;
     private float shotTime;
     private GameObject player;
+    private Material material;
 
     private void Start()
     {
+        material = GetComponent<MeshRenderer>().material;
         player = GameObject.FindWithTag("Player");
+        shotTime = shotInterval;
     }
 
     private void Update()
@@ -39,9 +42,11 @@ public class ProjectileSpawner : MonoBehaviour
                 Instantiate(prefab, spawnPoint.position, Quaternion.identity);
                 isActive = false;
                 shotTime = shotInterval;
+                material.color = Color.white;
             }
             else
             {
+                material.color = Color.red;
                 shotTime -= Time.deltaTime;
             }
         }
